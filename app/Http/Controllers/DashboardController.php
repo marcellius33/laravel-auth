520ce0c7f85ca0d4663ebbdf4df887fb818c34d9
Varrel Marcellius <marcellius33@gmail.com>
2024-05-24 16:08:49 +0700

    feat: dashboard with pagination

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\DashboardResource;
+use App\Http\Helpers\RequestHelper;
+use App\Http\Resources\UserCollection;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 /**
  * Dashboard
  */
 class DashboardController extends Controller
 {
-    public function __invoke(): DashboardResource
+    public function __invoke(Request $request): UserCollection
     {
-        return new DashboardResource(User::all());
+        $users = User::query();
+        return (new UserCollection($users->paginate(RequestHelper::limit($request))))
+            ->additional([
+                'total_users' => $users->count(),
+            ]);
     }
 }
